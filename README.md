4232855dfe0bcfea2bfc5c923fbfa9ecc93ff0c6
Ronald Jones <ronaldj@uark.edu>
2023-04-02 20:17:51 -0500

    Modifying write_pinnacle_slurm.py for assignment 5 and updating the README file

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Markdown_CV: Markdown assignment, submitted on 2/28/23
 assn-3.md: Assignment 3, submitted on 3/13/23
 
 write_pinnacle_slurm.py: Assignment 4 on 3/27/23
+
+write_pinncacle_slurm.py: modified for Assignment 5 on 4/2/23
--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -1,12 +1,30 @@
 #! /Users/RJ3/opt/anaconda3/bin/python3
 
-# Variables
+# Import desired modules
+import argparse
 
-job_name = '<JOB-NAME>'
-queue = 'comp01'
-walltime = 1
-num_nodes = 1
-num_processors = 24
+# Create argument parser object
+parser = argparse.ArgumentParser(description='This script returns a SLURM script with the passed arguments and options')
+
+# add positional arguments
+parser.add_argument('job', help = 'The name of the SLURM job', type = str)
+
+# Options for adding arguments
+parser.add_argument('-q', '-queue', help = 'Which HPC queue to utilize', default = 'comp72', type = str)
+parser.add_argument('-nodes', help = 'Number of HPC nodes to utilize on job', default = 1, type = int)
+parser.add_argument('-processors', help = 'Number of HPC processors to utilize on job', default = 1, type = int)
+parser.add_argument('-walltime', help = 'Max time allotted for job', default = 1, type = int)
+
+# Parse arguments
+args = parser.parse_args()
+
+# Set variables
+
+job_name = args.job
+queue = args.q
+walltime = args.nodes
+num_nodes = args.processors
+num_processors = args.walltime
 
 print('#!/bin/bash')
 
@@ -21,7 +39,7 @@ print('#SBATCH --time=' + str(walltime) + ':00:00')
 print('#SBATCH -o test_%j.out')
 print('#SBATCH -e test_%j.err')
 print('#SBATCH --mail-type=ALL')
-print('#SBATCH --mail-user=email@uark.edu')
+print('#SBATCH --mail-ronaldj@uark.edu')
 
 print()
 
@@ -30,4 +48,7 @@ print('module purge')
 print()
 
 print('cd $SLURM_SUBMIT_DIR')
+
+print()
+
 print('# job command here')

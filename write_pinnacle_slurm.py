f98511cb53c8b0819d37336fc476c98c1afe5edd
Ronald Jones <ronaldj@uark.edu>
2023-04-03 09:12:33 -0500

    Making adjustments to assignment 5 based off in-class feedback

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -4,38 +4,30 @@
 import argparse
 
 # Create argument parser object
-parser = argparse.ArgumentParser(description='This script returns a SLURM script with the passed arguments and options')
+parser = argparse.ArgumentParser(description="This script returns a SLURM file for jobs on the AHPCC cluster")
 
 # add positional arguments
-parser.add_argument('job', help = 'The name of the SLURM job', type = str)
+parser.add_argument("job_name", help = "The name of the SLURM job", type = str)
 
 # Options for adding arguments
-parser.add_argument('-q', '-queue', help = 'Which HPC queue to utilize', default = 'comp72', type = str)
-parser.add_argument('-nodes', help = 'Number of HPC nodes to utilize on job', default = 1, type = int)
-parser.add_argument('-processors', help = 'Number of HPC processors to utilize on job', default = 1, type = int)
-parser.add_argument('-walltime', help = 'Max time allotted for job', default = 1, type = int)
+parser.add_argument("-q", "--queue", help = "Requested queue (comp01, comp06, comp72)", default = "comp72", type = str)
+parser.add_argument("-n", "--num_nodes", help = "Number of HPC nodes to request", default = 1, type = int)
+parser.add_argument("-p", "--processors", help = "Number of HPC processors to utilize", default = 1, type = int)
+parser.add_argument("-w", "--walltime", help = "Length of the job", default = 1, type = int)
 
 # Parse arguments
 args = parser.parse_args()
-
-# Set variables
-
-job_name = args.job
-queue = args.q
-walltime = args.nodes
-num_nodes = args.processors
-num_processors = args.walltime
 
 print('#!/bin/bash')
 
 print()
 
-print('#SBATCH --job-name=' + job_name)
-print('#SBATCH --partition ' + queue)
-print('#SBATCH --nodes=' + str(num_nodes))
+print('#SBATCH --job-name=' +args.job_name)
+print('#SBATCH --partition ' + args.queue)
+print('#SBATCH --nodes=' + str(args.num_nodes))
 print('#SBATCH --qos comp')
-print('#SBATCH --tasks-per-node=' + str(num_processors))
-print('#SBATCH --time=' + str(walltime) + ':00:00')
+print('#SBATCH --tasks-per-node=' + str(args.num_processors))
+print('#SBATCH --time=' + str(args.walltime) + ':00:00')
 print('#SBATCH -o test_%j.out')
 print('#SBATCH -e test_%j.err')
 print('#SBATCH --mail-type=ALL')
